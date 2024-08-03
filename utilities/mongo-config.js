b4a5c1305b4f8e2f4042420b122c1e98c7b6a7b9
Lokesh Velayudham - lowkeyy.eth <lokesh.beyondx@gmail.com>
2024-08-02 22:31:23 -0400

    backend

--- utilities/mongo-config.js
+++ utilities/mongo-config.js
@@ -5,7 +5,7 @@ function mongodbConnection() {
   const username = process.env.DATABASE_USERNAME;
   const password = process.env.DATABASE_PASSWORD;
   const dbUrl = process.env.DATABASE_URL;
-  const dbName = "git-craft-" + "sg"; //change this to your name for maintaining different databases
+  const dbName = "git-craft-" + "new"; //change this to your name for maintaining different databases
   mongoose
     .connect(
       `mongodb+srv://${username}:${password}@${dbUrl}/${dbName}?retryWrites=true&w=majority&appName=Project`
